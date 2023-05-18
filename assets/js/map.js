3a5e75a6e2862dbf9249e4a38bf6ece8e57927ab
leet08 <50682021+leet08@users.noreply.github.com>
2023-05-17 21:10:09 -0400

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
              let map = new google.maps.Map(document.getElementById("map"), {
-                 zoom: 20,
+                 zoom: 1,
                  center: {
                      lat: 46.619261,
                      lng: -33.134766
